f223976a04e223e7134bf1549ba21f56800b3d10
tambonbon <nttammy@yahoo.com>
2021-02-16 17:04:12 +0100

    step 5.4

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -49,6 +49,11 @@ public class PostRepository {
     }
 
     public Post findById(Long id) {
+        for (Post post : ALL_POSTS) {
+            if (post.getId() == id) {
+                return post;
+            }
+        }
         return null;
     }
 }
