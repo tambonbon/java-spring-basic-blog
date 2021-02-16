1911a149ec4941d45d565d7c2b5bbd8dc5346aa4
tambonbon <nttammy@yahoo.com>
2021-02-16 14:58:39 +0100

    step 2.3

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -3,11 +3,14 @@ package com.pluralsight.blog;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class BlogController {
 
     @RequestMapping("/")
-    public String listPosts(){
+    public String listPosts(Map ModelMap){
+        ModelMap.put("title", "Blog Post 1");
         return "home";
     }
 
